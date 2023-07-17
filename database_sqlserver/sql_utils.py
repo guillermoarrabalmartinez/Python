368c19b51676f7bcf35ce4abddd8b0473565f67c
Guillermo Arrabal Martinez <guillermo.arrabalmartinez@axpo.com>
2023-07-17 18:09:24 +0200

    main tests file definition

--- database_sqlserver/sql_utils.py
+++ database_sqlserver/sql_utils.py
@@ -47,29 +47,30 @@ class Database:
             print(f'Error details: {str(e)}')       
 
 
-        def execute_sql_query(self, query: str):
-            with self.engine.connect() as connection:
-                connection.execute(query)
+    def execute_sql_query(self, query: str):
+        with self.engine.connect() as connection:
+            result = connection.execute(query)
+            return result
 
 
-        def import_sqlserver_fromfile(self, filetype: str,folder_path: str, file_name: str, table_name:str ,schema_name=None, if_exists='replace',column_types=None,sheet_name=None,use_columns=None):
-            """Function to connect and load a dataframe onto sqlserver from a file 
-            :param df: dataframe to load to sql
-            :param filetype: type of file to import (allowed: csv, xlsx)
-            :param table_name: name of the table is going to be created
-            :param schema_name: name of the schema in which the table will be allocated
-            :param if_exists: in case you want to append change that parameter (replace/append)
-            :param sheet_name: name of the sheet in the Excel file"""
+    def import_sqlserver_fromfile(self, filetype: str,folder_path: str, file_name: str, table_name:str ,schema_name=None, if_exists='replace',column_types=None,sheet_name=None,use_columns=None):
+        """Function to connect and load a dataframe onto sqlserver from a file 
+        :param df: dataframe to load to sql
+        :param filetype: type of file to import (allowed: csv, xlsx)
+        :param table_name: name of the table is going to be created
+        :param schema_name: name of the schema in which the table will be allocated
+        :param if_exists: in case you want to append change that parameter (replace/append)
+        :param sheet_name: name of the sheet in the Excel file"""
 
-            file_name = self.file
-            folder_path = self.folder_path
-            allowed_filetypes = self.allowed_filetypes
-            if filetype not in allowed_filetypes:
-                raise ValueError(f"Invalid filetype. Allowed filetypes: {', '.join(allowed_filetypes)}")
-                return  # Exit the function if an error is raised
-            elif filetype == 'csv':
-                df = ImportFiles.read_csv_file(folder_path+file_name)
-            elif filetype == 'xlsx' and sheet_name is not None:
-                df = ImportFiles.read_excel_file(folder_path+file_name,sheet_name = sheet_name)
-            else: print("something wrong")
-            Database.connect_and_load_to_sqlserver(df,table_name,schema_name,)
+        file_name = self.file
+        folder_path = self.folder_path
+        allowed_filetypes = self.allowed_filetypes
+        if filetype not in allowed_filetypes:
+            raise ValueError(f"Invalid filetype. Allowed filetypes: {', '.join(allowed_filetypes)}")
+            return  # Exit the function if an error is raised
+        elif filetype == 'csv':
+            df = ImportFiles.read_csv_file(folder_path+file_name)
+        elif filetype == 'xlsx' and sheet_name is not None:
+            df = ImportFiles.read_excel_file(folder_path+file_name,sheet_name = sheet_name)
+        else: print("something wrong")
+        Database.connect_and_load_to_sqlserver(df,table_name,schema_name,)
--- main_test.py
+++ main_test.py
@@ -9,7 +9,7 @@ from configuration.config import get_config
 # Invoke the get config from config.py 
 config = get_config()
 # Define instance parameters
-connection_string = config['SQL']['SUBDN748_CONNECTION_STRING']
+connection_string = config['SQL']['localhost_CONNECTION_STRING']
 file = config['Files']['PD1PFile']
 folderpath = config['Files']['FolderPath']
 allowed_filetypes = config['Files']['allowed_filetypes']
@@ -19,7 +19,9 @@ table =  os.path.splitext(file)[0]
 db = Database(connection_string, folderpath, file, allowed_filetypes)
 # Create an instance of the files class and pass the arguments.
 import_files = ImportFiles(folderpath + file)
-#Execute the function
+
+
+
 db.connect_and_load_to_sqlserver(import_files.read_csv_file(), table, schema,"replace")
 # test_result = db.execute_sql_query(f'SELECT TOP(5) * FROM {schema}.{table}')
 # Print the rows
